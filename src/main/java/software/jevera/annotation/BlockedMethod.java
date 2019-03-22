4681543183dffb5695b8c70cba15ac7342796ad0
Yaroslav <pivenjaroslav@gmail.com>
2019-03-22 17:59:42 +0200

    my annotation

---
+++ src/main/java/software/jevera/annotation/BlockedMethod.java
@@ -0,0 +1,20 @@
+package software.jevera.annotation;
+
+
+import software.jevera.domain.BankAccount;
+import software.jevera.service.bankaccount.BankAccountStateEnum;
+import software.jevera.service.bankaccount.StateMachine;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+@Target(value = ElementType.METHOD)
+@Retention(value= RetentionPolicy.RUNTIME)
+public @interface BlockedMethod {
+
+    BankAccountStateEnum value();
+
+}
